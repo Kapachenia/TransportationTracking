82c81ab2ab39d5efbe852ea82c5a5e9690283d8c
Yauheni Kapachenia <kapachenia@gmail.com>
2022-03-28 18:14:47 +0300

    package refactoring

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React, {StrictMode} from "react";
 import ReactDOM from "react-dom";
 import {Provider} from "react-redux";
-import {HashRouter} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import {store} from "./bll/store"
 import {App} from "./ui/App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
     <StrictMode>
-        <HashRouter basename={'TransportationTracking'}>
+        <BrowserRouter basename={'TransportationTracking'}>
             <Provider store={store}>
                 <App/>
             </Provider>
-        </HashRouter>
+        </BrowserRouter>
     </StrictMode>,
     rootElement
 );
